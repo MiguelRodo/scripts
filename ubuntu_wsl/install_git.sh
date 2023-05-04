c34e9339c06706052026c3210b6e6cfbd8f318e1
Miguel  Rodo <rdxmig002@myuct.ac.za>
2023-05-04 14:04:34 +0200

    Update Git install commands

--- ubuntu_wsl/install_git.sh
+++ ubuntu_wsl/install_git.sh
@@ -6,4 +6,8 @@ git config --global user.name "Miguel  Rodo"
 git config --global user.email "rdxmig002@myuct.ac.za"
 git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
 git config --global init.defaultBranch main
+# to reconcile pulls
+git config pull.ff only
+
+
 
