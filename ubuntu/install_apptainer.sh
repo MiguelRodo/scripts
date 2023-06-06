45af908866962e1e4ba98b291243951a39b35e72
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-06-06 11:51:58 +0000

    Remove setuid part from apptainer install

--- ubuntu/install_apptainer.sh
+++ ubuntu/install_apptainer.sh
@@ -5,7 +5,4 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:apptainer/ppa
 sudo apt update
 sudo apt install -y apptainer
-sudo add-apt-repository -y ppa:apptainer/ppa
-sudo apt update
-sudo apt install -y apptainer-suid
 
